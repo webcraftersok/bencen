484f86bbee9b3f07b918ee558cd646d8e925be29
rodg22 <giudicerodrigo@hotmail.com>
2024-05-08 09:45:17 -0300

    fix: comments deleted

--- src/app/components/FadeInComponent/FadeInComponent.tsx
+++ src/app/components/FadeInComponent/FadeInComponent.tsx
@@ -4,16 +4,16 @@ import { useInView } from "react-intersection-observer";
 
 const FadeInComponent = ({ children }: any) => {
   const { ref, inView } = useInView({
-    triggerOnce: true, // Solo activar una vez
-    threshold: 0.5, // El elemento se activa cuando está al menos un 50% visible
+    triggerOnce: true,
+    threshold: 0.5,
   });
 
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, y: 50 }} // Inicialmente transparente y desplazado hacia abajo
-      animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }} // Fade in y desplazamiento
-      transition={{ duration: 0.5 }} // Duración de la transición
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
+      transition={{ duration: 0.5 }}
     >
       {children}
     </motion.div>
