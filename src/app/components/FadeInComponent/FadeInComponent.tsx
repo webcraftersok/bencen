c0ea13b085bb43089ffb731c7a2868f7b5d47585
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2024-05-27 08:11:17 -0300

    fix: fix animation fadeIn

--- src/app/components/FadeInComponent/FadeInComponent.tsx
+++ src/app/components/FadeInComponent/FadeInComponent.tsx
@@ -5,7 +5,7 @@ import { useInView } from "react-intersection-observer";
 const FadeInComponent = ({ children }: any) => {
   const { ref, inView } = useInView({
     triggerOnce: true,
-    threshold: 0.5,
+    threshold: 0.2,
   });
 
   return (
